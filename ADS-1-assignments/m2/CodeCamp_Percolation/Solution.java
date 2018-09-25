4b5b3dd7da546746a75cc4c3d72ac1016025b87f
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-25 18:17:47 +0530

    "check_style_score -> 3 of 24 passed. style score: 0/1 "

--- ADS-1-assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1-assignments/m2/CodeCamp_Percolation/Solution.java
@@ -11,104 +11,142 @@
 // You can implement the above API to solve the problem
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for percolation.
+ */
 class Percolation {
-	int[][] arr;
-	WeightedQuickUnionUF wqu;
-	// int[][] sz;
-	int count;
-	int n;
-	Percolation(int n) {
+    int[][] arr;
+    WeightedQuickUnionUF wqu;
+    // int[][] sz;
+    int count;
+    int n;
+    /**
+     * Constructs the object.
+     *
+     * @param      n     size of the grid
+     */
+    Percolation(int n) {
        arr = new int[n][n];
        // sz = new int[n][n];
        count = 0;
        n = n;
        wqu = new WeightedQuickUnionUF((n * n) + 2);
     }
-    public String toString() {
-    	for (int i = 0; i < arr.length; i++) {
-    		for (int j = 0; j < arr.length; j++) {
-    			System.out.print(arr[i][j]);
-    		}
-    		System.out.println();
-    	}
-    	return "";
-    }
-	public void open(int row, int col) {
-		int count = 0;
-		row = row - 1;
-		col = col - 1;
-		arr[row][col] = 1;
-			count++;
-		if (row == 0) {
-			wqu.union(0, component(row, col));
+    /**
+     * to open the block.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    public void open(int row, int col) {
+        int count = 0;
+        row = row - 1;
+        col = col - 1;
+        arr[row][col] = 1;
+            count++;
+        if (row == 0) {
+            wqu.union(0, component(row, col));
         }
         if (row == n - 1) {
-        	wqu.union(((n * n) + 1), component(row, col));
+            wqu.union(((n * n) + 1), component(row, col));
 
         }
         if (row > 0) {
-        	if (arr[row + 1][col] == 1) {
-        		wqu.union(component(row, col), component(row + 1, col));
-        	}
+            if (arr[row + 1][col] == 1) {
+                wqu.union(component(row, col), component(row + 1, col));
+            }
         }
         if (row <= n-1) {
-        	if (arr[row - 1][col] == 1) {
-        		wqu.union(component(row, col), component(row - 1, col));
-        	}
+            if (arr[row - 1][col] == 1) {
+                wqu.union(component(row, col), component(row - 1, col));
+            }
         }
         if (col > 0) {
-        	if (arr[row][col - 1] == 1) {
-        		wqu.union(component(row, col), component(row, col + 1));
-        	}
+            if (arr[row][col - 1] == 1) {
+                wqu.union(component(row, col), component(row, col + 1));
+            }
         }
         if (col <= n-1) {
-        	if (arr[row][col + 1] == 1) {
-        		wqu.union(component(row, col), component(row, col - 1));
-        	}
+            if (arr[row][col + 1] == 1) {
+                wqu.union(component(row, col), component(row, col - 1));
+            }
         }
 
     }
+    /**
+     * to get the component
+     *
+     * @param      i     row
+     * @param      j     col
+     *
+     * @return     integer value
+     */
     public int component(int i, int j) {
-    	return ((i * n) + j) + 1;
+        return ((i * n) + j) + 1;
     }
-	public boolean isOpen(int row, int col) {
-		return arr[row - 1][col - 1] == 1;
+    /**
+     * Determines if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(int row, int col) {
+        return arr[row - 1][col - 1] == 1;
 
-	}
-	public boolean isFull(int row, int col) {
-		return arr[row - 1][col - 1] == 0;
     }
-	public int numberofOpenSites() {
-		return count;
+    /**
+     * Determines if full.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if full, False otherwise.
+     */
+    public boolean isFull(int row, int col) {
+        return arr[row - 1][col - 1] == 0;
     }
-	public boolean percolates() {
-		return wqu.connected(0, (n * n) + 1);
+    /**
+     * count number of open sites.
+     *
+     * @return    no of open sites
+     */
+    public int numberofOpenSites() {
+        return count;
+    }
+    /**
+     * to check whether percolates or not.
+     *
+     * @return     boolean value
+     */
+    public boolean percolates() {
+        return wqu.connected(0, (n * n) + 1);
 
-	}
+    }
 }
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    Solution() {
 
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int inp = sc.nextInt();
-		Percolation p = new Percolation(inp);
-		while (sc.hasNext()) {
-			p.open(sc.nextInt(), sc.nextInt());
-		}
-		System.out.println(p.percolates());
-		}
-	
-}
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inp = sc.nextInt();
+        Percolation p = new Percolation(inp);
+        while (sc.hasNext()) {
+            p.open(sc.nextInt(), sc.nextInt());
+        }
+        System.out.println(p.percolates());
+        }
+    }
