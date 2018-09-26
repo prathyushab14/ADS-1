e767b310514501a622b3fb98ff1e88ab9c6b07de
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-26 19:45:01 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m3/3-sum/Solution.java
+++ ADS-1-assignments/m3/3-sum/Solution.java
@@ -1,34 +1,55 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.*;
+/**
+ * Class for solution.
+ */
 class Solution {
-	int[] arr;
-	int n;
-	Solution() {
-		arr = new int[n];
-	}
-	public static void threeSum(int[] array) {
-		Arrays.sort(array);
-		// System.out.println(Arrays.toString(array));
-		int count = 0;
-		for (int i = 0; i < array.length; i++) {
-			for (int j = i + 1; j < array.length; j++) {
-				for (int k = j + 1; k < array.length; k++) {
-			    if (array[i] + array[j] + array[k] == 0) {
-			    	count++;
-			    }
-			}
-		}
-	}
-		System.out.println(count);
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int inp = sc.nextInt();
-		int[] arr = new int[inp];
-		for (int i = 0; i < inp; i++) {
-			arr[i] = sc.nextInt();
-		}
-		threeSum(arr);
-	}
+    private int[] arr;
+    private int n;
+    /**
+     * Constructs the object.
+     */
+    Solution() {
+        arr = new int[n];
+    }
+    public int size() {
+        return n;
+    }
+    public int[] array() {
+        return arr;
+    }
+    /**
+     * three sum.
+     *
+     * @param      array  The array
+     */
+    public static void threeSum(final int[] array) {
+        Arrays.sort(array);
+        // System.out.println(Arrays.toString(array));
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                for (int k = j + 1; k < array.length; k++) {
+                if (array[i] + array[j] + array[k] == 0) {
+                    count++;
+                }
+            }
+        }
+    }
+        System.out.println(count);
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inp = sc.nextInt();
+        int[] arr = new int[inp];
+        for (int i = 0; i < inp; i++) {
+            arr[i] = sc.nextInt();
+        }
+        threeSum(arr);
+    }
 }
