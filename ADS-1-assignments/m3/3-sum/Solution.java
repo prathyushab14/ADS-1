756267a218489ca13e5e01e8fbcfe61e6a3fc301
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-26 19:34:33 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m3/3-sum/Solution.java
+++ ADS-1-assignments/m3/3-sum/Solution.java
@@ -7,21 +7,34 @@ class Solution {
 	Solution() {
 		arr = new int[n];
 	}
-	public static void threeSum(int[] array) {
-		Arrays.sort(array);
-		// System.out.println(Arrays.toString(array));
-		int count = 0;
-		for (int i = 0; i < array.length; i++) {
-			for (int j = i + 1; j < array.length; j++) {
-				for (int k = j + 1; i < array.length; k++) {
-			    if ((array[i] + array[j] + array[k]) == 0) {
-			    	count++;
-			    }
-			}
-		}
-	}
-		System.out.println(count);
-	}
+	// public static void threeSum(int[] array) {
+	// 	Arrays.sort(array);
+	// 	// System.out.println(Arrays.toString(array));
+	// 	int count = 0;
+	// 	for (int i = 0; i < array.length; i++) {
+	// 		for (int j = i + 1; j < array.length; j++) {
+	// 			for (int k = j + 1; i < array.length; k++) {
+	// 		    if (array[i] + array[j] + array[k] == 0) {
+	// 		    	count++;
+	// 		    }
+	// 		}
+	// 	}
+	// }
+	// 	System.out.println(count);
+	// }
+	public static int threesum(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                for (int k = j + 1; k < array.length; k++) {
+                    if (array[i] + array[j] + array[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int inp = sc.nextInt();
@@ -29,6 +42,6 @@ class Solution {
 		for (int i = 0; i < inp; i++) {
 			arr[i] = sc.nextInt();
 		}
-		threeSum(arr);
+		System.out.println(threesum(arr));
 	}
 }
