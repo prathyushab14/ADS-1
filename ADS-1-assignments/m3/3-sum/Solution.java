969e4deef760c43b75716468c6e0a12544ab0b52
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-26 19:26:15 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m3/3-sum/Solution.java
+++ ADS-1-assignments/m3/3-sum/Solution.java
@@ -12,11 +12,13 @@ class Solution {
 		int count = 0;
 		for (int i = 0; i < array.length; i++) {
 			for (int j = i + 1; j < array.length; j++) {
-			    if (array[i] == -(array[j])) {
+				for (int k = i + 1; i < array.length; k++) {
+			    if ((array[i] + array[j] + array[k]) == 0) {
 			    	count++;
 			    }
 			}
 		}
+	}
 		System.out.println(count);
 	}
 	public static void main(String[] args) {
