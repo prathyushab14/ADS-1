aca828ed2736707e85098a06d0d400b87fceeb70
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-09-26 20:37:41 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m3/Sorted_Array/Solution.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Solution {
+	Solution() {
+
+    }
+    public static void sortArr(int[] array1, int[] array2) {
+    	int[] arr3 = new int[array1.length + array2.length];
+    	int s = array1.length + array2.length;
+    	int f = 0;
+    	int l = 0;
+    	int i = 0;
+    	while (f < array1.length && l < array2.length) {
+    		if (array1[f] < array2[l]) {
+    			arr3[i++] = array1[f];
+            } else if (array1[f] > array2[l]) {
+                arr3[i++] = array2[l];
+            }
+            if (array1.length > array2.length) {
+            		arr3[i++] = array1[f++];
+            }
+            if (array2.length > array1.length) {
+            		arr3[i++] = array2[l++];
+            }
+        }
+    }
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int arr1size = sc.nextInt();
+    	int arr2size = sc.nextInt();
+    	int[] arr1 = new int[arr1size];
+    	int[] arr2 = new int[arr2size];
+    	sc.nextLine();
+    	String[] array1 = sc.nextLine().split(",");
+    	String[] array2 = sc.nextLine().split(",");
+    	for (int i = 0; i < arr1size; i++) {
+    		arr1[i] = Integer.parseInt(array1[i]);
+    	}
+    	for (int i = 0; i < arr2size; i++) {
+    		arr2[i] = Integer.parseInt(array2[i]);
+    	}
+    	sortArr(arr1, arr2);
+
+    }
+}
