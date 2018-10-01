d84fe50c94d12799b0bf9e22b5227e84477edada
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-01 10:30:20 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m7/7.1 Steque/Solution.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Node {
+	int data;
+	Node next;
+	Node(int data) {
+		this.data = data;
+	}
+}
+class Steque {
+	Node start;
+	int size;
+	Steque() {
+		start = null;
+		size = 0;
+	}
+	public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = start;
+        start = newNode;
+        size++;
+        display();
+    }
+    public void pop() {
+    	if (size == 0) {
+            System.out.println("Steque is empty");
+            return;
+        } else {
+        start = start.next;
+        size--;
+        display();
+        }
+    }
+    public void enqueue(int data) {
+    	Node newNode = new Node(data);
+    	Node temp = start;
+    	while (temp.next != null) {
+    		temp = temp.next;
+    	}
+    	temp.next = newNode;
+    	size++;
+    	display();
+    }
+	public void display() {
+        if (size == 0) {
+            System.out.println("[]");
+        } else {
+        Node temp = start;
+        while (temp.next != null) {
+        	System.out.print(temp.data+", ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+    }
+     
+}
+class Solution {
+	Solution() {
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Steque s = new Steque();
+		int noofTest = Integer.parseInt(sc.nextLine());
+		while (sc.hasNext()) {
+			String[] str = sc.nextLine().split(" ");
+			switch (str[0]) {
+				case "push":
+				s.push(Integer.parseInt(str[1]));
+				break;
+				case "pop":
+				s.pop();
+				break;
+				case "enqueue":
+				s.enqueue(Integer.parseInt(str[1]));
+				break;
+			}
+		}
+		
+	}
+}
