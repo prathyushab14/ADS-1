d4e785af14e6eeedb9348830daf998adf7fc736d
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-01 10:49:04 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- ADS-1-assignments/m7/7.1 Steque/Solution.java
+++ ADS-1-assignments/m7/7.1 Steque/Solution.java
@@ -62,20 +62,39 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		Steque s = new Steque();
 		int noofTest = Integer.parseInt(sc.nextLine());
-		while (sc.hasNext()) {
-			String[] str = sc.nextLine().split(" ");
-			switch (str[0]) {
-				case "push":
-				s.push(Integer.parseInt(str[1]));
-				break;
-				case "pop":
-				s.pop();
-				break;
-				case "enqueue":
-				s.enqueue(Integer.parseInt(str[1]));
-				break;
+		if (noofTest > 1) {
+		    while (sc.hasNext()) {
+			    String[] str = sc.nextLine().split(" ");
+			    switch (str[0]) {
+				    case "push":
+				    s.push(Integer.parseInt(str[1]));
+				    break;
+				    case "pop":
+				    s.pop();
+				    break;
+				    case "enqueue":
+				    s.enqueue(Integer.parseInt(str[1]));
+				    break;
+				}
 			}
+		} else {
+		    for (int i = 0; i < noofTest; i++) {
+		    	Steque st = new Steque();
+		    	while (sc.hasNext()) {
+		    		String[] str = sc.nextLine().split(" ");
+			        switch (str[0]) {
+				    case "push":
+				    s.push(Integer.parseInt(str[1]));
+				    break;
+				    case "pop":
+				    s.pop();
+				    break;
+				    case "enqueue":
+				    s.enqueue(Integer.parseInt(str[1]));
+				    break;
+				}
+            }
 		}
-		
-	}
+    }
+}
 }
