cf24b2bc44f7d4044810a64a166d0aa0598e99d4
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-02 19:07:19 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m8/8.2 Team Ranking With Selection/CricTeam.java
@@ -0,0 +1,48 @@
+class CricTeam implements Comparable<CricTeam>{
+	String name;
+	int wins;
+	int losses;
+	int draws;
+	CricTeam(String name, String wins, String losses, String draws) {
+		this.name = name;
+		this.wins = Integer.parseInt(wins);
+		this.losses = Integer.parseInt(losses);
+		this.draws = Integer.parseInt(draws);
+	}
+	public String getName() {
+		return name;
+	}
+	public int getWins() {
+		return wins;
+	}
+	public int getLosses() {
+		return losses;
+	}
+	public int getDraws() {
+		return draws;
+	}
+	public int compareTo(CricTeam c) {
+		if (this.wins > c.wins) {
+			return 1;
+		}
+		if (this.wins < c.wins) {
+			return -1;
+		}
+		if (this.losses < c.losses) {
+			return 1;
+		}
+		if (this.losses > c.losses) {
+			return -1;
+		}
+		if (this.draws > c.draws) {
+			return 1;
+		}
+		if (this.draws < c.draws) {
+			return -1;
+		}
+		return 0;  
+	}
+	public String toString() {
+		return this.name;
+	}
+}
---
+++ ADS-1-assignments/m8/8.2 Team Ranking With Selection/Teams.java
@@ -0,0 +1,35 @@
+class Teams {
+	private CricTeam[] teams;
+	private int size;
+	Teams() {
+		teams = new CricTeam[10];
+		size = 0;
+	}
+	public void addCricketTeam(CricTeam t) {
+		teams[size++] = t;
+	}
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < size; i++) {
+			s += teams[i].name+",";
+		}
+		return s.substring(0, s.length() - 1);
+	}
+	public void exch(CricTeam[] teams, int i, int j) {
+		CricTeam ct = teams[j];
+		teams[j] = teams[i];
+		teams[i] = ct; 
+	}
+	public void InsertionSort() {
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = i + 1; j > 0; j--) {
+				if (less(teams, j - 1, j)) {
+					exch(teams, j -1, j);
+				} 
+			}
+        }
+	}
+	public boolean less(CricTeam[] arr, int i, int j) {
+		return  arr[i].compareTo(arr[j]) > 0;
+	}
+}
