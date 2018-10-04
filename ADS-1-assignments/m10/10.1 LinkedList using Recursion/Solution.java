edfabbc8aa481fb9f07b29bdc2b7ffa8ba321d4b
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-04 18:27:21 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
     }
     /**
      * main function.
@@ -14,31 +14,32 @@ class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	LinkedList l = new LinkedList();
-    	while(sc.hasNext()) {
-    		String data = sc.nextLine();
-    		String[] tokens = data.split(" ");
-    	    switch (tokens[0]) {
-    		    case "insertAt":
-    		    try {
-    		        l.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
-    		        l.display();
-    		    } catch (Exception e) {
-    			    System.out.println("Can't insert at this position.");
-    		    }
-    		    break;
-    		    case "reverse":
-    		    try {
-    		        l.reverse();
-    		        l.display();
-    		    } catch (Exception e) {
-    			    System.out.println("No elements to reverse.");
-    		    }
-    		    break;
-    		    default:
-    		    break;
-    		}
+        Scanner sc = new Scanner(System.in);
+        LinkedList l = new LinkedList();
+        while (sc.hasNext()) {
+            String data = sc.nextLine();
+            String[] tokens = data.split(" ");
+            switch (tokens[0]) {
+                case "insertAt":
+                try {
+                    l.insertAt(Integer.parseInt(tokens[1]),
+                        Integer.parseInt(tokens[2]));
+                    l.display();
+                } catch (Exception e) {
+                    System.out.println("Can't insert at this position.");
+                }
+                break;
+                case "reverse":
+                try {
+                    l.reverse();
+                    l.display();
+                } catch (Exception e) {
+                    System.out.println("No elements to reverse.");
+                }
+                break;
+                default:
+                break;
+            }
         }
     }
 }
