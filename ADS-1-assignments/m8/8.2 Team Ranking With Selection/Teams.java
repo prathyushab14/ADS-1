3b86d69fa6995bf96d577678b2aa87c123653b9f
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-04 15:52:25 +0530

    "ads1_m8_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m8/8.2 Team Ranking With Selection/Teams.java
+++ ADS-1-assignments/m8/8.2 Team Ranking With Selection/Teams.java
@@ -30,12 +30,14 @@ class Teams {
 	public void InsertionSort() {
 		for (int i = 0; i < size - 1; i++) {
 			for (int j = i + 1; j > 0; j--) {
-				if (less(teams, j - 1, j)) {
-					exch(teams, j -1, j);
+				if (!less(teams, j - 1, j)) {
+					break;
+				} else {
+					exch(teams, j - 1, j);
+				}
 				} 
 			}
         }
-	}
 	public boolean less(CricTeam[] arr, int i, int j) {
 		return  arr[i].compareTo(arr[j]) < 0;
 	}
