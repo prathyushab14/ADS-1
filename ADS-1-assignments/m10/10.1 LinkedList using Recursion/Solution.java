7658d87be293c2c45c162bd6f78ab8a1e41cea0a
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-04 18:25:33 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1-assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,59 +1,19 @@
-import java.util.*;
-class Node {
-	int data;
-	Node next;
-	Node(int data) {
-		this.data = data;
-	}
-}
-class LinkedList {
-	Node head;
-	int size;
-	LinkedList() {
-		head = null;
-		size = 0;
-	}
-	public void insertAt(int pos, int ele) throws Exception{
-		Node newNode = new Node(ele);
-		if (pos < 0 || pos > size) {
-			throw new Exception();
-	    }
-        head = insertAt(pos, head, newNode, 0);
-    }
-    public Node insertAt(int pos, Node first, Node obj, int count) {
-    	if (pos == count) {
-    		obj.next = first;
-    		size++;
-    		return obj;
-        }
-    	first.next = insertAt(pos, first.next, obj, count + 1);
-    	return first;
-    }
-	public void reverse() {
-		reverse(null, head);
-	}
-	void reverse(Node previous, Node current) {
-		if (current != null) {
-			reverse(current, current.next);
-			current.next = previous;
-		} else {
-			head = previous;
-		}
-	}
-	void display() {
-		Node temp = head;
-		String str = "";
-		while (temp != null) {
-			str += temp.data+", ";
-			temp = temp.next;
-        }
-        System.out.println(str.substring(0,str.length()-2));
-	}
-}
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	Solution() {
     }
-    public static void main(String[] args) {
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
     	Scanner sc = new Scanner(System.in);
     	LinkedList l = new LinkedList();
     	while(sc.hasNext()) {
