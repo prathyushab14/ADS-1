cf9ed3e6fdd86607bef12962ef652db812423cf9
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-06 11:03:16 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m12/Assignment-1/Solution.java
+++ ADS-1-assignments/m12/Assignment-1/Solution.java
@@ -5,18 +5,19 @@ class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Merit m = new Merit();
-        while (sc.hasNext()) {
         	int noOfapplied = sc.nextInt();
         	int noOfvacancies = sc.nextInt();
         	int noOfunres = sc.nextInt();
         	int noOfBC = sc.nextInt();
         	int noOfSC = sc.nextInt();
         	int noOfST = sc.nextInt();
+        	int vacancies = noOfvacancies;
+
+        while (sc.hasNext()) {
         	String data = sc.nextLine();
         	String[] tokens = data.split(",");
         	Student s = new Student(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]);
         	m.addStudent(s);
-        	int vacancies = noOfvacancies;
         }
         m.InsertionSort();
         System.out.println(m);
