6545ca4182d64665c1ac5107f4adbe96c1ea8529
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-06 11:39:04 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m12/Assignment-1/Student.java
+++ ADS-1-assignments/m12/Assignment-1/Student.java
@@ -9,6 +9,10 @@ class Student {
 	Student(String name, String dob, String s1, String s2, String s3, String total, String category) {
 		this.name = name;
 		this.dob = dob;
+		String[] date = dob.split("-");
+		int year = Integer.parseInt(date[2]);
+		int month = Integer.parseInt(date[1]);
+		int day = Integer.parseInt(date[0]);
 		this.s1 = Integer.parseInt(s1);
 		this.s2 = Integer.parseInt(s2);
 		this.s3 = Integer.parseInt(s3);
@@ -24,8 +28,20 @@ class Student {
 	public String getCategory() {
         return category;
 	}
-	public String getdob() {
-		return dob;
+	public int getYear() {
+		String[] date = dob.split("-");
+		int year = Integer.parseInt(date[2]);
+		return year;
+	}
+	public int getMonth() {
+		String[] date = dob.split("-");
+		int month = Integer.parseInt(date[1]);
+		return month;
+	}
+	public int getDay() {
+		String[] date = dob.split("-");
+		int day = Integer.parseInt(date[0]);
+		return day;
 	}
 	public int compareTo(Student s) {
 		if (this.total > s.total) {
@@ -46,12 +62,24 @@ class Student {
 		if (this.s2 < s.s2) {
 			return -1;
 		}
-		// if (this.dob < s.dob) {
-		// 	return 1;
-		// }
-		// if (this.dob > s.dob) {
-  //           return -1;
-		// }
+		if (this.getYear() < s.getYear()) {
+			return 1;
+		}
+		if (this.getYear() > s.getYear()) {
+			return -1;
+		}
+		if (this.getMonth() < s.getMonth()) {
+            return 1;
+		}
+		if (this.getMonth() > s.getMonth()) {
+            return -1;
+		}
+		if (this.getDay() < s.getDay()) {
+			return 1;
+		}
+		if (this.getDay() > s.getDay()) {
+			return -1;
+		}
 		return 0;
 	}
 	public String toString() {
