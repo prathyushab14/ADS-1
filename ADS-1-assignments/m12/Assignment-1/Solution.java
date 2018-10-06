aef72da234197a0a4ac4dc90bba485436bb227d3
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-06 11:14:05 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m12/Assignment-1/Solution.java
+++ ADS-1-assignments/m12/Assignment-1/Solution.java
@@ -5,19 +5,18 @@ class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Merit m = new Merit();
-        	int noOfapplied = sc.nextInt();
-        	int noOfvacancies = sc.nextInt();
-        	int noOfunres = sc.nextInt();
-        	int noOfBC = sc.nextInt();
-        	int noOfSC = sc.nextInt();
-        	int noOfST = sc.nextInt();
-        	int vacancies = noOfvacancies;
-
         while (sc.hasNext()) {
-        	String data = sc.nextLine();
-        	String[] tokens = data.split(",");
-        	Student s = new Student(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]);
-        	m.addStudent(s);
+        	int noOfapplied = Integer.parseInt(sc.nextLine());
+        	int noOfvacancies = Integer.parseInt(sc.nextLine());
+        	int noOfunres = Integer.parseInt(sc.nextLine());
+        	int noOfBC = Integer.parseInt(sc.nextLine());
+        	int noOfSC = Integer.parseInt(sc.nextLine());
+        	int noOfST = Integer.parseInt(sc.nextLine());
+        	for (int i = 0; i < noOfapplied; i++) {
+        	    String data = sc.nextLine();
+        	    String[] tokens = data.split(",");
+        	    m.addStudent(new Student(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]));
+        	}
         }
         m.InsertionSort();
         System.out.println(m);
