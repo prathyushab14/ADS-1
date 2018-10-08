c77c4d678d815d61dfebf475bd022b1d59049137
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-08 20:11:30 +0530

    "ads1_m13_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m13/13.1 isMinHeap/MinHeap.java
+++ ADS-1-assignments/m13/13.1 isMinHeap/MinHeap.java
@@ -24,7 +24,7 @@ public class MinHeap<Key extends Comparable<Key>> {
     public boolean less(int i, int j) {
     	System.out.println(pq[i]);
     	System.out.println(pq[j]);
-    	return pq[i].compareTo(pq[j]) >= 0;
+    	return pq[i].compareTo(pq[j]) > 0;
     }
     public boolean check() {
 		int k = n - 1;
