3185971ce5211d527e9189f9ab1646029c0d9d7c
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-13 13:01:48 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m18/Week-3_Assignment-1/Solution.java
+++ ADS-1-assignments/m18/Week-3_Assignment-1/Solution.java
@@ -24,32 +24,43 @@ class Stock implements Comparable<Stock>{
     	return this.name.compareTo(that.name);
     }
 }
-class BinarySearchST {
-    BinarySearchST() {
-    }
-}
 class Query {
 	Stock[] stocks;
+	String[] keys;
+	Float[] values;
 	int size;
 	Query() {
 		stocks = new Stock[200];
 		size = 0;
     }
-    public float get(Stock key) {
-    		int i = rank(key);
-            if (i < size && stocks[i].getName().compareTo(key.name) == 0) {
-                return stocks[i].getValue();
-     	    }
-     	    return 0;
-        }
-    public int rank(final Stock key) {
+    public void add(Stock st) {
+    	stocks[size++] = st;
+    }
+    public float get(String key) {
+    	int i = rank(key);
+        if (i < size && stocks[i].getName().compareTo(key) == 0) {
+            return stocks[i].getValue();
+     	}
+     	return 0;
+    }
+    // public Stock intersection() {
+    // 	for (Stock s : minpq) {
+    // 		for (Stock st : maxPQ) {
+    // 			if (s.equals(st)) {
+    // 				return s;
+    // 			}
+    // 		}
+    // 	}
+    // 	return null;
+    // }
+    public int rank(final String key) {
         if (key == null) {
             throw new IllegalArgumentException("argument to rank() is null");
         }
         int lo = 0, hi = size - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            int cmp = key.compareTo(stocks[mid]);
+            int cmp = key.compareTo(stocks[mid].name);
             if (cmp < 0) {
                 hi = mid - 1;
             } else if (cmp > 0) {
@@ -60,23 +71,44 @@ class Query {
         }
         return lo;
     }
+    public void insert(final String k, final float v) {
+        if (k == null) {
+            throw new IllegalArgumentException("Null key");
+        }
+        int i = rank(k);
+        if (i < size && keys[i].compareTo(k) == 0) {
+            stocks[i].change = v;
+            return;
+        }
+        for (int j = size; j > i; j--) {
+            keys[j] = keys[j - 1];
+            values[j] = values[j - 1];
+        }
+        keys[i] = k;
+        values[i] = v;
+        size++;
+    }
 }
 class Solution {
 	Solution() {
     }
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
+    	Query q = new Query();
     	MinPQ<Stock> min = new MinPQ<Stock>();
     	MaxPQ<Stock> max = new MaxPQ<Stock>();
     	int n = Integer.parseInt(sc.nextLine());
-    	for (int i = 0; i < 6 * n; i++) {
-    		String data = sc.nextLine();
-    		String[] tokens = data.split(",");
-    		Stock s = new Stock(tokens[0], tokens[1]);
-    		//System.out.println(Arrays.toString(tokens));
-    		max.insert(s);
-    		max.insert(s);
+    	while (sc.hasNext()) {
+    	    for (int i = 0; i < n; i++) {
+    		    String data = sc.nextLine();
+    		    String[] tokens = data.split(",");
+    		    Stock s = new Stock(tokens[0], tokens[1]);
+    		    //System.out.println(Arrays.toString(tokens));
+    		    q.add(s);
+    		}
         }
+
+
         int noque = Integer.parseInt(sc.nextLine());
     }
 }
