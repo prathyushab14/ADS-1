bc76de1844e4c50d8fb220d8deaca0e575ad89f5
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-22 17:27:55 +0530

    "ads1_m19_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m19/19.1 BST/Solution.java
+++ ADS-1-assignments/m19/19.1 BST/Solution.java
@@ -356,7 +356,7 @@ class BinaryST {
             return select(x.getRight(), k - t - 1); 
         }
         // else {
-            return x;
+            return x.getRight();
     // }
         }
 }
