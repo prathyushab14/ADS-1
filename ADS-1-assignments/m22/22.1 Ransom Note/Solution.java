d891a5918f92f7dd6936042fb464edb032ace9b1
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-25 12:22:07 +0530

    "ads1_m22_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignments/m22/22.1 Ransom Note/Solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class RansomNote {
+	String[] mag;
+	String[] notee;
+	RansomNote() {
+
+	}
+	RansomNote(int m, int n) {
+		mag = new String[m];
+		notee = new String[n];
+    }
+    public boolean check() {
+    	for (String s : notee) {
+    		for (String st : mag) {
+    			if (!s.equals(st)) {
+    				return false;
+    			}
+    		}
+    	}
+    	return true;
+    }
+}
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		RansomNote rn = new RansomNote();
+		while (sc.hasNext()) {
+			int m = sc.nextInt();
+			int n = sc.nextInt();
+			String magazine = sc.nextLine();
+			String[] magdata = magazine.split(" ");
+			String note = sc.nextLine();
+			String[] notedata = note.split(" ");
+			rn = new RansomNote(magdata.length, notedata.length);
+		}
+		System.out.println(rn.check());
+    }
+}
