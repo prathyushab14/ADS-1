099a9a5b3d75f31a3d395d94071e1e89786fbf34
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-26 14:13:23 +0530

    "ads1_m7_a1: 2/2 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m7/7.1 Steque/Steque.java
+++ ADS-1-assignments/m7/7.1 Steque/Steque.java
@@ -2,41 +2,51 @@
  * Class for steque.
  */
 class Steque {
-	/**
+    /**
     * Class for node.
     */
     class Node {
         /**
         * data.
         */
-        int data;
+        private int data;
         /**
         * item.
         */
-        Node next;
+        private Node next;
         /**
         * Constructs the object.
         *
         * @param      dataa  The data
         */
-        Node(int dataa) {
+        Node(final int dataa) {
             this.data = dataa;
         }
+        /**
+         * Gets the next.
+         *
+         * @return     The next.
+         */
         public Node getNext() {
             return next;
         }
+        /**
+         * Gets the data.
+         *
+         * @return     The data.
+         */
         public int getData() {
-        	return data;
+            return data;
         }
     }
     /**
      * start node.
      */
-    Node start;
+    private Node start;
     /**
      * size of steque.
      */
-    int size;
+    private int size;
     /**
      * Constructs the object.
      */
@@ -44,11 +54,21 @@ class Steque {
         start = null;
         size = 0;
     }
+    /**
+     * Gets the size.
+     *
+     * @return     The size.
+     */
     public int getSize() {
-    	return size;
+        return size;
     }
+    /**
+     * Gets the start.
+     *
+     * @return     The start.
+     */
     public Node getStart() {
-    	return start;
+        return start;
     }
     /**
      * the operations in the function take constant time.
