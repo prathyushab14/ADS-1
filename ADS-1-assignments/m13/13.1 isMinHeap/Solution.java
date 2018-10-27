75c31005659c313bfa3232293e03f93957a70893
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-27 19:10:24 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m13/13.1 isMinHeap/Solution.java
+++ ADS-1-assignments/m13/13.1 isMinHeap/Solution.java
@@ -45,6 +45,17 @@ class MinHeap<Key extends Comparable<Key>> {
          */
         this.n = array.length;
     }
+    // /**
+    //  * Initializes an empty priority queue with the given initial capacity,
+    //  * using the given comparator.
+    //  * @param  initCapacity the initial capacity of this priority queue
+    //  * @param  comparatorr the order in which to compare the keys
+    //  */
+    // public MinHeap(final int initCapacity, final Comparator<Key> comparatorr) {
+    //     this.comparator = comparatorr;
+    //     pq = (Key[]) new Object[initCapacity + 1];
+    //     n = 0;
+    // }
     /**
      * Time complexity of this method is O(1).
      */
@@ -110,6 +121,104 @@ class MinHeap<Key extends Comparable<Key>> {
         }
         return true;
     }
+    /**
+     * Returns a smallest key on this priority queue.
+     * @return a smallest key on this priority queue.
+     * time complexity is O(1)
+     */
+    public Key min() {
+        if (isEmpty()) {
+            return null;
+        }
+        return pq[1];
+    }
+    // /**
+    //  * Removes and returns a smallest key on this priority queue.
+    //  * @return a smallest key on this priority queue
+    //  * time complexity is O(log(n))
+    //  */
+    // public Key delMin() {
+    //     if (isEmpty()) {
+    //         return null;
+    //     }
+    //     Key min = pq[1];
+    //     exch(1, n--);
+    //     sink(1);
+    //     pq[n + 1] = null;
+    //     final int four = 4;
+    //     if ((n > 0) && (n == (pq.length - 1) / four)) {
+    //         resize(pq.length / 2);
+    //     }
+    //     return min;
+    // }
+    // /**
+    //  * swim method.
+    //  * @param      k    index.
+    //  * time complexity is O(log(n))
+    //  */
+    // private void swim(final int k) {
+    //     int k1 = k;
+    //     while (k1 > 1 && greater(k1 / 2, k1)) {
+    //         exch(k1, k1 / 2);
+    //         k1 = k1 / 2;
+    //     }
+    // }
+    /**
+     * exch method to swap the elements.
+     * @param      i     index.
+     * @param      j     index.
+     * time complexity is O(1)
+     */
+    private void exch(final int i, final int j) {
+        Key swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+    }
+    /**
+     * resize method to resize the array.
+     * @param      capacity  The capacity
+     */
+    private void resize(final int capacity) {
+        assert capacity > n;
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 1; i <= n; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
+    }
+    // /**
+    //  * greater method.
+    //  * @param      i     index.
+    //  * @param      j     index.
+    //  * @return     true or false.
+    //  * time complexity is O(1)
+    //  */
+    // private boolean greater(final int i, final int j) {
+    //     if (comparator == null) {
+    //         return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
+    //     } else {
+    //         return comparator.compare(pq[i], pq[j]) > 0;
+    //     }
+    // }
+    // /**
+    //  * sink method.
+    //  * @param      k    index.
+    //  * time complexity is O(log(n))
+    //  */
+    // private void sink(final int k) {
+    //     int k1 = k;
+    //     while (2 * k1 <= n) {
+    //         int j = 2 * k1;
+    //         if (j < n && greater(j, j + 1)) {
+    //             j++;
+    //         }
+    //         if (!greater(k1, j)) {
+    //             break;
+    //         }
+    //         exch(k1, j);
+    //         k1 = j;
+    //     }
+    // }
 }
 /**
  * Solution class.
