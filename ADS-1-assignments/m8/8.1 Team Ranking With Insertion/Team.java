48113041933a43f24173b03953535c8575359962
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-27 17:23:46 +0530

    "ads1_m8_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Team.java
+++ ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Team.java
@@ -6,76 +6,85 @@
  * Class for team.
  */
 class Team {
-	private CricketTeam[] teams;
-	private int size;
-	Team() {
-		teams = new CricketTeam[10];
-		size = 0;
-	}
-	/**
-	 * Adds a cricket team.
-	 *
-	 * @param      t     { parameter_description }
-	 */
-	public void addCricketTeam(CricketTeam t) {
-		teams[size++] = t;
-	}
-	/**
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		String s = "";
-		for (int i = 0; i < size; i++) {
-			s += teams[i].name+",";
-		}
-		return s.substring(0, s.length() - 1);
-	}
-	/**
-	 * swaps two items.
-	 * time complexity is O(K)
-	 *
-	 * @param      teams  The teams
-	 * @param      i      { parameter_description }
-	 * @param      j      { parameter_description }
-	 */
-	public void exch(CricketTeam[] teams, int i, int j) {
-		//System.out.println(";;;;;;");
-		CricketTeam ct = teams[j];
-		teams[j] = teams[i];
-		teams[i] = ct; 
-	}
-	/**
-	 * sorts the given elements.
-	 * time complexity of selection sort in worst case is O(n^2)
-	 */
-	public void selectionSort() {
-		//System.out.println("-------");
-		for (int i = 0; i < size - 1; i++) {
-			int min = i;
-			for (int j = i + 1; j < size; j++) {
-				//System.out.println("[[[[[[");
-				if (less(teams, j, min)) {
-					min = j;
-				} 
-			}
-			exch(teams, i, min);
-			//System.out.println(teams[i]);
+    /**
+     * teams array.
+     */
+    private CricketTeam[] teams;
+    /**
+     * size.
+     */
+    private int size;
+    /**
+     * Constructs the object.
+     */
+    Team() {
+        teams = new CricketTeam[10];
+        size = 0;
+    }
+    /**
+     * Adds a cricket team.
+     *
+     * @param      t     { parameter_description }
+     */
+    public void addCricketTeam(final CricketTeam t) {
+        teams[size++] = t;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < size; i++) {
+            s += teams[i].name+",";
         }
-	}
-	/**
-	 * compare two items.
-	 * time complexity is O(1)
-	 *
-	 * @param      arr   The arr
-	 * @param      i     integer
-	 * @param      j     integer
-	 *
-	 * @return     true or false
-	 */
-	boolean less(CricketTeam[] arr, int i, int j) {
-		//System.out.println("0000000");
-		return  arr[i].compareTo(arr[j]) > 0;
-	}
+        return s.substring(0, s.length() - 1);
+    }
+    /**
+     * swaps two items.
+     * time complexity is O(K)
+     *
+     * @param      teams  The teams
+     * @param      i      { parameter_description }
+     * @param      j      { parameter_description }
+     */
+    public void exch(final CricketTeam[] teams, final int i, final int j) {
+        //System.out.println(";;;;;;");
+        CricketTeam ct = teams[j];
+        teams[j] = teams[i];
+        teams[i] = ct;
+    }
+    /**
+     * sorts the given elements.
+     * time complexity of selection sort in worst case is O(n^2)
+     */
+    public void selectionSort() {
+        //System.out.println("-------");
+        for (int i = 0; i < size - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < size; j++) {
+                //System.out.println("[[[[[[");
+                if (less(teams, j, min)) {
+                    min = j;
+                }
+            }
+            exch(teams, i, min);
+            //System.out.println(teams[i]);
+        }
+    }
+    /**
+     * compare two items.
+     * time complexity is O(1)
+     *
+     * @param      arr   The arr
+     * @param      i     integer
+     * @param      j     integer
+     *
+     * @return     true or false
+     */
+    boolean less(final CricketTeam[] arr, final int i, final int j) {
+        //System.out.println("0000000");
+        return  arr[i].compareTo(arr[j]) > 0;
+    }
 }
