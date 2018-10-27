8039b44c9df6763c0d7ee092004ca7d84b9b75a8
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-27 11:44:51 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignments/m24/2/Solution.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+/**
+ * Class for student.
+ */
+class Student {
+    /**
+     * rollnum.
+     */
+    private String rollno;
+    /**
+     * name.
+     */
+    private String name;
+    /**
+     * total marks.
+     */
+    private double total;
+    /**
+     * Constructs the object.
+     */
+    Student() {
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param      rollno1  The rollno
+     * @param      name1    The name
+     * @param      total1  The total
+     */
+    Student(final String rollno1, final String name1, final double total1) {
+        rollno = rollno1;
+        name = name1;
+        total = total1;
+    }
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Gets the rollno.
+     *
+     * @return   The rollno.
+     */
+    public String gerRoll() {
+        return rollno;
+    }
+    /**
+     * Gets the total.
+     *
+     * @return     The total.
+     */
+    public Double getmar() {
+        return total;
+    }
+}
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BinaryST bs = new BinaryST();
+		Student s = new Student();
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+        	 String str = sc.nextLine();
+             String[] tokens = str.split(",");
+             s = new Student(tokens[0], tokens[1], Double.parseDouble(tokens[2]));
+             bs.put(Double.parseDouble(tokens[1]), tokens[0]);
+            }
+		int m = Integer.parseInt(sc.nextLine());
+	    for (int i = 0; i < m; i++) {
+            String que = sc.nextLine();
+            String[] query = que.split(" ");
+            switch (query[0]) {
+            	case "BE":
+            	System.out.println(bs.keys(query[1], query[2]));
+            	break;
+            	// case "LE":
+            	// break;
+            	// case "GE":
+            	// break;
+            }
+
+		}
+
+	}
+}
