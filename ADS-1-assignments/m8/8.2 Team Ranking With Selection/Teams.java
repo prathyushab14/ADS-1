616807290ffcaf80a7003c05cbd1c7d622f08997
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-10-27 18:45:21 +0530

    "ads1_m8_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m8/8.2 Team Ranking With Selection/Teams.java
+++ ADS-1-assignments/m8/8.2 Team Ranking With Selection/Teams.java
@@ -6,39 +6,43 @@
  * Class for teams.
  */
 class Teams {
-	private CricTeam[] teams;
-	private int size;
-	Teams() {
-		teams = new CricTeam[10];
-		size = 0;
-	}
-	public void addCricketTeam(CricTeam t) {
-		teams[size++] = t;
-	}
-	public String toString() {
-		String s = "";
-		for (int i = 0; i < size; i++) {
-			s += teams[i].name+",";
-		}
-		return s.substring(0, s.length() - 1);
-	}
-	public void exch(CricTeam[] teams, int i, int j) {
-		CricTeam ct = teams[j];
-		teams[j] = teams[i];
-		teams[i] = ct; 
-	}
-	public void InsertionSort() {
-		for (int i = 0; i < size - 1; i++) {
-			for (int j = i + 1; j > 0; j--) {
-				if (!less(teams, j - 1, j)) {
-					break;
-				} else {
-					exch(teams, j - 1, j);
-				}
-				} 
-			}
+    private CricTeam[] teams;
+    private int size;
+    Teams() {
+        teams = new CricTeam[10];
+        size = 0;
+    }
+    public void addCricketTeam(final CricTeam t) {
+        teams[size++] = t;
+    }
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < size; i++) {
+            s += teams[i].name+",";
         }
-	public boolean less(CricTeam[] arr, int i, int j) {
-		return  arr[i].compareTo(arr[j]) < 0;
-	}
+        return s.substring(0, s.length() - 1);
+    }
+    public void exch(final CricTeam[] teams1, final int i, final int j) {
+        CricTeam ct = teams1[j];
+        teams[j] = teams1[i];
+        teams[i] = ct; 
+    }
+    /**
+     * sorting the elements.
+     * time complexity of insertion sort is O(N^2) in worst case.
+     */
+    public void InsertionSort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (!less(teams, j - 1, j)) {
+                    break;
+                } else {
+                    exch(teams, j - 1, j);
+                }
+                } 
+            }
+        }
+    public boolean less(final CricTeam[] arr, final int i, final int j) {
+        return  arr[i].compareTo(arr[j]) < 0;
+    }
 }
